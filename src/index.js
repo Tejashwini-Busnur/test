5e876bb06fe3702d1371f2dd4b06eca0d015e542
Tejashwini-Busnur <tejashwini.b.19@gmail.com>
2020-06-20 19:08:15 -0400

    component added

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Clock from './Components/Clock';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Clock />
   </React.StrictMode>,
   document.getElementById('root')
 );
